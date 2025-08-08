0fbacceba58a9a61df39d36adab9072d6ad22b35
erratir <mail4entry@gmail.com>
2025-08-08 16:29:13 +0300

    vite config

--- examples/vue/vite.config.js
+++ examples/vue/vite.config.js
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      }
+    }
+  }
 })
